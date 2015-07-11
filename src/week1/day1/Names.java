c0019655a793eccbe53eb68ee45b0cb6a53aa79c
Tordlin <tordlin@yandex.ru>
2015-07-11 19:55:12 +0300

    Initial commit

---
+++ src/week1/day1/Names.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+/**
+ * Created by Tordlin on 11/07/2015.
+ */
+public class Names {
+    public static void main(String[] args) {
+        String names = "1-Oleg, 2-Ivan, 3-Kostya, 4-Andrey";
+
+        String evenNames = Names.nameEvenReturns(names);
+
+        System.out.println(evenNames);
+    }
+
+    public static String nameEvenReturns(String names){
+
+        return null;
+    }
+}
