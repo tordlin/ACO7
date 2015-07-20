c0f99cda49b3da9afe6a10d0ecc0cdae8399a300
Tordlin <tordlin@yandex.ru>
2015-07-20 23:55:43 +0300

    hw2 v1.5

---
+++ src/week2/hw2/StringClass.java
@@ -0,0 +1,12 @@
+package week2.hw2;
+
+/**
+ * Created by Tordlin on 20/07/2015.
+ */
+public class StringClass {
+    char[] elements;
+
+    public StringClass(String word) {
+        
+    }
+}
---
+++ src/week2/hw2/TestStringClass.java
@@ -0,0 +1,10 @@
+package week2.hw2;
+
+/**
+ * Created by Tordlin on 20/07/2015.
+ */
+public class TestStringClass {
+    public static void main(String[] args) {
+        StringClass stringClass = new StringClass("Word");
+    }
+}
