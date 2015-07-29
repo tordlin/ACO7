dc2bebb73f8d07f10e6331346b0f5c290f85351a
Tordlin <tordlin@yandex.ru>
2015-07-29 22:50:52 +0300

    w3hw3.1

--- src/week3/hw3/AcroJuggler.java
+++ src/week3/hw3/AcroJuggler.java
@@ -3,7 +3,7 @@ package week3.hw3;
 /**
  * Created by Tordlin on 29/07/2015.
  */
-public class AcroJuggler extends Employee{
+public class AcroJuggler extends Acrobat{
     @Override
     public void perform() {
 
